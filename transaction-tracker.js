449d58908d5d4f1bb57b6371e72f2b119a2dd961
Nielsen, Clinton <clinton_nielsen@intuit.com>
2023-11-04 14:06:03 -0700

    Initial commit

---
+++ transaction-tracker.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import fs_extra from "fs-extra";
+const { readdirSync, lstatSync, existsSync, mkdirSync, removeSync, copySync, lutimesSync } = fs_extra;
+
+const vendorToCategoryMap = {};
+
+(() => {
+    // Read in the current statements
+    // Classify them
+    // Output the totals
+})();
